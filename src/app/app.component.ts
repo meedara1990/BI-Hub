8826249c7eaea40a38e4c996a84ee417a7b794cd
Mouni <mounikaeedara@gmail.com>
2024-03-03 02:12:32 -0500

    Adding Sample Header Body & Footer files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbNavModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    NgbDropdownModule,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
+  ]
 })
 export class AppComponent {
   title = 'bi-hub';
