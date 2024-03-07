a76c9142c7433ba0433edd1a0aa3700632edcc1d
Poornima Padmanaban <poornima.padmanaban@accenture.com>
2024-03-07 18:44:02 +0530

    new changes

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//import json from './dashboards.json';
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.scss'
 })
-export class CardsComponent {}
+export class CardsComponent implements OnInit {
+   navItems: any;
+
+   constructor(private httpClient: HttpClient) { }
+
+    ngOnInit() {
+      
+    }
+
+   
+
+}
