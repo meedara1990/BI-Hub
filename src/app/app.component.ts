e94704232d52f075206fbe84991d4c554b84a36a
Mouni <mounikaeedara@gmail.com>
2024-03-08 00:20:42 -0500

    Additional changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,22 @@ import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { CardsComponent } from './cards/cards.component';
-import { MainDashboardComponent } from "./main-dashboard/main-dashboard.component";
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [
-        RouterOutlet,
-        NgbDropdownModule,
-        HeaderComponent,
-        BodyComponent,
-        FooterComponent,
-        CardsComponent,
-        MainDashboardComponent
-    ]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    NgbDropdownModule,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    CardsComponent,
+    MainDashboardComponent
+  ]
 })
 export class AppComponent {
   title = 'bi-hub';
--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
-import {
-  FaIconLibrary,
-  FontAwesomeModule
-} from '@fortawesome/angular-fontawesome';
-import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import * as dashboards from '../../assets/dashboards.json';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-dashboard',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [CommonModule],
   templateUrl: './main-dashboard.component.html',
   styleUrl: './main-dashboard.component.scss'
 })
 export class MainDashboardComponent {
-  constructor(library: FaIconLibrary) {
-    library.addIcons(faChartLine);
-  }
+  public dashboards = dashboards;
 }
