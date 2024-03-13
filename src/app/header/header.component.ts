76ec154495b1f9183e76887ee5d44fcc8eeb6756
Poornima Padmanaban <poornima.padmanaban@accenture.com>
2024-03-13 21:13:34 +0530

    add

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  isOpened: boolean = false;
+  
+  div1:boolean=true;
+  div2:boolean=true;
+  div3:boolean=true;
 
-  toggle() {
-    this.isOpened = !this.isOpened;
+  div1Function(){
+      this.div1=true;
+      this.div2=false;
+      this.div3=false
+  }
+
+  div2Function(){
+      this.div2=true;
+      this.div1=false;
+      this.div3=false
+  }
+
+  div3Function(){
+      this.div3=true;
+      this.div2=false;
+      this.div1=false
   }
 }
