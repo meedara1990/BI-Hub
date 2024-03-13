d69e900e685e2d8b9c5ac3e44752e8614377ae8e
Poornima Padmanaban <poornima.padmanaban@accenture.com>
2024-03-13 21:26:50 +0530

    add

--- src/app/dashboards/dashboards.component.ts
+++ src/app/dashboards/dashboards.component.ts
@@ -1,18 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { Dashboard } from '../types/responses/Dashboard';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-dashboards',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbNavModule],
   templateUrl: './dashboards.component.html',
   styleUrl: './dashboards.component.scss'
 })
 export class DashboardsComponent implements OnInit {
   @Input() id!: string;
   dashboard?: Dashboard;
-
+	active = 1;
   ngOnInit(): void {
     this.dashboard = history.state;
     console.log('Dashbaord', this.dashboard);
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,32 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgbCollapseModule],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  isCollapsed = false;
   
-  div1:boolean=true;
-  div2:boolean=true;
-  div3:boolean=true;
-
-  div1Function(){
-      this.div1=true;
-      this.div2=false;
-      this.div3=false
-  }
-
-  div2Function(){
-      this.div2=true;
-      this.div1=false;
-      this.div3=false
-  }
-
-  div3Function(){
-      this.div3=true;
-      this.div2=false;
-      this.div1=false
-  }
 }
