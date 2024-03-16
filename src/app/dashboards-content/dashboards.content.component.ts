4cb52b86ef34be5db0181addfd6ebeb81a948c1d
Poornima Padmanaban <poornima.padmanaban@accenture.com>
2024-03-16 15:44:46 +0530

    'card'

---
+++ src/app/dashboards-content/dashboards.content.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-dashboards-content',
+  standalone: true,
+  imports: [CommonModule, NgbNavModule],
+  templateUrl: './dashboards.content.component.html',
+  styleUrl: './dashboards.content.component.scss'
+})
+export class DashboardsContentComponent implements OnInit {
+  active = 1;
+
+  ngOnInit(): void {
+    
+  }
+}
